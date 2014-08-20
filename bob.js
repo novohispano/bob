b56d3f72d4f503b8e0a0ee375cf1b8983e9d9cf8
novohispano <novohispano@gmail.com>
2014-08-19 22:46:47 -0600

    Final implementation of Bob

---
+++ bob.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var Bob = function() {
+  var silence = function(saying) {
+    return saying.trim() === '';
+  };
+
+  var shouting = function(saying) {
+    return saying.toUpperCase() === saying;
+  };
+
+  var question = function(saying) {
+    var lastLetterIndex = saying.length - 1
+
+    return saying[lastLetterIndex] === "?"
+  };
+
+  this.hey = function(saying) {
+    if (silence(saying)) {
+      return "Fine. Be that way!";
+    } else if (shouting(saying)) {
+      return "Woah, chill out!";
+    } else if (question(saying)) {
+      return "Sure.";
+    } else {
+      return "Whatever.";
+    };
+  };
+
+  this.isTeenager = function(saying) {
+    return true;
+  };
+
+  this.isAdult = function(saying) {
+    return !this.isTeenager();
+  };
+};
+
+module.exports = Bob;
---
+++ bob_test.spec.js
@@ -0,0 +1,53 @@
+var Bob = require('./bob');
+
+describe("Bob", function () {
+  var bob = new Bob();
+
+  it("states something", function() {
+    var result = bob.hey('Tom-ay-to, tom-aaaah-to.');
+
+    expect(result).toEqual('Whatever.');
+  });
+
+  it("shouts", function() {
+    var result = bob.hey('WATCH OUT!');
+
+    expect(result).toEqual('Woah, chill out!');
+  });
+
+  it("asks a question", function() {
+    var result = bob.hey('Does this cryogenic chamber make me look fat?');
+
+    expect(result).toEqual('Sure.');
+  });
+
+  it("shouts numbers", function() {
+    var result = bob.hey('1, 2, 3 GO!');
+
+    expect(result).toEqual('Woah, chill out!');
+  });
+
+  it("is quiet", function () {
+    var result = bob.hey('');
+
+    expect(result).toEqual('Fine. Be that way!');
+  });
+
+  it("prolonged silence", function () {
+    var result = bob.hey('   ');
+
+    expect(result).toEqual('Fine. Be that way!');
+  });
+
+  it("knows he is a teenager", function() {
+    var result = bob.isTeenager();
+
+    expect(result).toBe(true);
+  })
+
+  it("knows he is not an adult", function() {
+    var result = bob.isAdult();
+
+    expect(result).toBe(false);
+  })
+});
