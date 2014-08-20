72f84a2d80f57f3e80467baa28b151d0c7a71887
novohispano <novohispano@gmail.com>
2014-08-20 09:41:35 -0600

    boiler plate

--- bob_test.spec.js
+++ bob_test.spec.js
@@ -3,63 +3,33 @@ var Bob = require('./bob');
 describe('Bob', function () {
   var bob = new Bob();
 
-  it('states something', function() {
-    var result = bob.hey('Tom-ay-to, tom-aaaah-to.');
-
-    expect(result).toEqual('Whatever.');
+  xit('states something', function() {
   });
 
-  it('states something else', function() {
-    var result = bob.hey('I\'m to fancy.');
-
-    expect(result).toEqual('Whatever.');
+  xit('states something else', function() {
   });
 
-  it('shouts', function() {
-    var result = bob.hey('WATCH OUT!');
-
-    expect(result).toEqual('Woah, chill out!');
+  xit('shouts', function() {
   });
 
-  it('shouts numbers', function() {
-    var result = bob.hey('1, 2, 3 GO!');
-
-    expect(result).toEqual('Woah, chill out!');
+  xit('shouts numbers', function() {
   });
 
-  it('asks a question', function() {
-    var result = bob.hey('Does this cryogenic chamber make me look fat?');
-
-    expect(result).toEqual('Sure.');
+  xit('asks a question', function() {
   });
 
-  it('asks another question', function() {
-    var result = bob.hey('Do you like JavaScript?');
-
-    expect(result).toEqual('Sure.');
+  xit('asks another question', function() {
   });
 
-  it('is quiet', function () {
-    var result = bob.hey('');
-
-    expect(result).toEqual('Fine. Be that way!');
+  xit('is quiet', function () {
   });
 
-  it('is quiet longer', function () {
-    var result = bob.hey('   ');
-
-    expect(result).toEqual('Fine. Be that way!');
+  xit('is quiet longer', function () {
   });
 
-  it('knows he is a teenager', function() {
-    var result = bob.isTeenager();
-
-    expect(result).toBe(true);
+  xit('knows he is a teenager', function() {
   })
 
-  it('knows he is not an adult', function() {
-    var result = bob.isAdult();
-
-    expect(result).toBe(false);
+  xit('knows he is not an adult', function() {
   })
 });
